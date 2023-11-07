2494fa5624246087941267e71f3321cedaf3645c
Githaiga <pythongeeko@gmail.com>
2023-11-07 12:06:57 +0300

    drf-test

--- TestRepo/app1/views.py
+++ TestRepo/app1/views.py
@@ -8,5 +8,5 @@ from rest_framework.views import APIView
 
 class HelloWorld(APIView):
     def get(self, request):
-        data = {'message': 'Hello, World!'}
+        data = {'message': 'Hello, World! Universe'}
         return Response(data)
